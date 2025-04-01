737135c5a3a60aa87ad94ed801e64adf03cb8642
ragul <141637129+ragultv@users.noreply.github.com>
2025-04-01 17:11:32 +0530

    Add files via upload

---
+++ LinkedListAddition.java
@@ -0,0 +1,58 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) { this.val = val; }
+}
+
+public class LinkedListAddition {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0); 
+        ListNode current = dummyHead; 
+        int carry = 0; 
+
+        
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry; 
+
+            if (l1 != null) { 
+                sum += l1.val;
+                l1 = l1.next; 
+            }
+
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next; 
+            }
+
+            carry = sum / 10; 
+            current.next = new ListNode(sum % 10); 
+            current = current.next; 
+        }
+
+        return dummyHead.next; 
+    }
+    
+    
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        // Test case
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        
+        LinkedListAddition solution = new LinkedListAddition();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+        printList(result); 
+    }
+}
